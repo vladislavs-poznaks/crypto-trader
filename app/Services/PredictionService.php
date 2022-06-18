1eb48f946c8296c5d72324b163263f03f2f5987b
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2022-06-18 20:38:38 +0300

    Updated prediction service, interface, filled factory, added unit tests

--- app/Services/PredictionService.php
+++ app/Services/PredictionService.php
@@ -6,32 +6,36 @@ use App\Constants\Code;
 use App\Constants\Range;
 use App\Models\Candlestick;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Model;
 
 class PredictionService implements PredictionServiceInterface
 {
     public function __construct(
         private Code   $code,
         private float  $price,
-        private Carbon $datetime
+        private Carbon $datetime,
+        private ?Candlestick $candlestick = null,
     ) {}
 
-    public function buy(): bool
+    public function shouldBuy(): bool
     {
         $candlestick = $this->getCandlestick();
 
         return $candlestick->open / $this->price > 1.05;
     }
 
-    public function sell(): bool
+    public function shouldSell(): bool
     {
         $candlestick = $this->getCandlestick();
 
-        return $candlestick->open / $this->price < 0.90;
+        return $candlestick->open / $this->price < 0.95;
     }
 
-    protected function getCandlestick(): ?Model
+    protected function getCandlestick()
     {
+        if ($this->candlestick) {
+            return $this->candlestick;
+        }
+
         return Candlestick::query()
             ->where('code', $this->code)
             ->where('range', Range::MONTH)
--- database/factories/CandlestickFactory.php
+++ database/factories/CandlestickFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Constants\Code;
+use App\Constants\Range;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,30 @@ class CandlestickFactory extends Factory
      */
     public function definition()
     {
+        $code = collect(Code::cases())->random();
+        $range = collect(Range::cases())->random();
+
+        $open = $this->faker->randomFloat(8);
+        $close = $this->faker->randomFloat(8);
+
         return [
-            //
+            'code' => $code,
+            'range' => $range,
+            'open' => $open,
+            'close' => $close,
+            'close_open_difference' => $close - $open,
+            'low' => $open - $open * 1.05,
+            'high' => $close + $close * 1.05,
+            'high_low_difference' => ($close + $close * 1.05) - ($open - $open * 1.05),
+            'volume' => $this->faker->randomNumber(6),
+            'open_time' => now()->subMonth(),
+            'close_time' => now()->subMonths(2),
+            'asset_volume' => $this->faker->randomNumber(6),
+            'base_volume' => $this->faker->randomNumber(6),
+            'trades' => $this->faker->randomNumber(6),
+            'asset_buy_volume' => $this->faker->randomNumber(6),
+            'taker_buy_volume' => $this->faker->randomNumber(6),
+            'ignored' => false,
         ];
     }
 }
